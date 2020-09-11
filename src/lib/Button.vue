ddfac14364a75aa21dec9d22fab3944b1ac5cac9
wanghuan <196912334@qq.com>
2020-09-11 16:36:48 +0800

    add button

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,13 +1,188 @@
 <template>
-  <div>我是Button</div>
+  <button class="hhw-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="hhw-loadingIndicator"></span>
+    <slot />
+  </button>
 </template>
-
 <script lang="ts">
+import { computed } from "vue";
 export default {
-  name: "Button"
-}
+  props: {
+    theme: {
+      type: String,
+      default: "button",
+    },
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return {
+        [`hhw-theme-${theme}`]: theme,
+        [`hhw-size-${size}`]: size,
+        [`hhw-level-${level}`]: level,
+      };
+    });
+    return { classes };
+  },
+};
 </script>
-
-<style scoped>
-
+<style lang="scss">
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+$red: red;
+$grey: grey;
+.hhw-button {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
+  & + & {
+    margin-left: 8px;
+  }
+  &:hover,
+  &:focus {
+    color: $blue;
+    border-color: $blue;
+  }
+  &:focus {
+    outline: none;
+  }
+  &::-moz-focus-inner {
+    border: 0;
+  }
+  &.hhw-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  &.hhw-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
+  &.hhw-size-big {
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.hhw-size-small {
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.hhw-theme-button {
+    &.hhw-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    &.hhw-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  &.hhw-theme-link {
+    &.hhw-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.hhw-theme-text {
+    &.hhw-level-main {
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    &.hhw-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  &.hhw-theme-button {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+      &:hover {
+        border-color: $grey;
+      }
+    }
+  }
+  &.hhw-theme-link, &.hhw-theme-text {
+    &[disabled] {
+      cursor: not-allowed;
+      color: $grey;
+    }
+  }
+  > .hhw-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: hhw-spin 1s infinite linear;
+  }
+}
+@keyframes hhw-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
+import './lib/hhw.scss'
+import './index.scss'
 import {createApp} from 'vue'
 // @ts-ignore
 import App from './App.vue'
-import './index.scss'
 // @ts-ignore
 import router from './router/index.ts'
 
