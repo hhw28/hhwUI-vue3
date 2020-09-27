d57d8e76cc0f9bfd90be72f86fc3bb00b928b1bc
wanghuan <196912334@qq.com>
2020-09-27 16:49:00 +0800

    import md文件编辑器报错

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,5 +2,10 @@ declare module "*.vue" {
   import { ComponentOptions } from "vue";
   const ComponentOptions: ComponentOptions;
   export default ComponentOptions;
+
+}
+declare module '*.md' {
+  const str: string
+  export default str
 }
 
